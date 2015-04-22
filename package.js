6fdde3304bf38a12531bbcd6eb4981c4faf8a0b7
Daniel C <333222@gmail.com>
2015-04-22 19:21:30 +0800

    Avoid polluting global namespace publish v0.3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'meteorspark:logger',
   summary: 'Wrapper around jonnyreeves/js-logger',
-  version: '0.1.0',
+  version: '0.3.0',
   git: 'https://github.com/MeteorSpark/meteor-logger'
 });
 
@@ -12,7 +12,7 @@ client = 'client';
 Package.onUse(function (api) {
   api.versionsFrom('0.9.4');
 
-  api.add_files('js-logger/src/logger.js', both);
+  api.add_files('src/logger.js', both);
 
   api.export('Logger');
 });
